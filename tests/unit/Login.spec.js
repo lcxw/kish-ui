cc8a6cc9ecd20f6df193b8f8ef6763ae0b575553
coremail-wxia <1358208301@qq.com>
2019-07-22 10:39:46 +0800

    add travis ci build  file.

---
+++ tests/unit/Login.spec.js
@@ -0,0 +1,10 @@
+import { expect } from "chai";
+import request from "request";
+import { baseUrl } from "../../src/utils/global";
+// import "../../src/mock/index";
+
+describe("addClass", function() {
+  it("should add class to element", function() {
+    console.log("un used test fule");
+  });
+});
